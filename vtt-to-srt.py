411e236df484f7d46486311aa666a688648bf9bb
snhansen <45770265+snhansen@users.noreply.github.com>
2019-04-26 19:39:16 +0200

    Some minor stylistic changes

--- vtt-to-srt.py
+++ vtt-to-srt.py
@@ -1,30 +1,35 @@
 import sys
 import codecs
+import os.path
 
 filename = sys.argv[1]
 
-with open(filename,encoding='utf8') as f:
+with open(filename, encoding='utf8') as f:
     sub = [x.strip() for x in f.readlines() if 'WEBVTT' not in x]
 
 while (not sub[0]):
     del sub[0]
-    
-def istimerow(x):
-    return ('-->' in x) & (x.count(':') == 4) & (x.count('.') == 2)
+
+
+def is_time_row(x):
+    return '-->' in x and x.count(':') == 4 and x.count('.') == 2
+
 
 index = 0
 subno = 0
 n = len(sub)
 
 while (index < n):
-    if istimerow(sub[index]):
-        sub[index] = sub[index].replace('.',',')
-        sub.insert(index,str(subno))
+    if is_time_row(sub[index]):
+        sub[index] = sub[index].replace('.', ',')
+        sub.insert(index, str(subno))
         index += 1
         subno += 1
         n += 1
     index += 1
 
-with codecs.open(filename.split('vtt')[0] + 'srt', 'w', 'utf-8') as f:
+
+with codecs.open(os.path.splitext(filename)[0] + '.srt', 'w', 'utf-8') as f:
     for row in sub:
-        f.write("%s\n" % row)
+        f.write(f'{row}\n')
+
